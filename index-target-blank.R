f7157da6971de7861c1bf5ba6acd7b1712c28c5a
bcdanl <bchoe@geneseo.edu>
2025-02-17 04:56:23 -0500

    lec07

--- index-target-blank.R
+++ index-target-blank.R
@@ -15,7 +15,7 @@ system(paste("rm", shQuote("index.html")))
 lec_dates <- c('0122',
                '0127', '0129',
                '0203', '0205',
-               '0210')
+               '0210', '0217')
 lec_n <- length(lec_dates)
 
 lec_nos <- 1:lec_n
